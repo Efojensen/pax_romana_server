14bcdd593a98a4097d6866c029a6e480a2f611db
EfoJensen <Jensena231@gmail.com>
2026-02-05 21:22:45 +0000

    finished index.js file re-write

--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
+import cors from 'cors'
 import express from 'express';
 
 const app = express();
-const port = 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const port = process.env['SERVER_PORT'];
+
+const allowedOrigins = [`http://localhost:${port}`, 'https://pax-romana-knust.vercel.app'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true // Enable credentials (cookies, etc.) if needed
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello World from Bun & Express!');
 });
 
 app.listen(port, () => {
-    console.log(`Express server listening on http://localhost:${port}`);
+    console.log(`Pax Romana Server is live on http://localhost:${port}`);
 });
