270ebac7e5c444c4785f064b2067f6e410b44d5d
EfoJensen <Jensena231@gmail.com>
2026-03-13 16:46:11 +0000

    tested out and verified the functionality of the getAdmins endpoint

--- sql/admins/getAllAdmins.ts
+++ sql/admins/getAllAdmins.ts
@@ -1,18 +1,18 @@
 import pool from '../../config/db';
+import type { ReturnedCitizen } from '../../types/citizen';
 
-const getAllAdminsQuery = async () => {
+const getAllAdminsQuery = async (): Promise<ReturnedCitizen[]> => {
     try {
-        const query = `SELECT id, firstname, lastname, username, email, photo_url
-                        FROM admin;`;
+        const query = `SELECT c.name, c.email, c.phone_number, p.name AS programme, c.level FROM citizens AS c
+            INNER JOIN admins AS a ON a.member_id = c.id
+            INNER JOIN programmes AS p ON c.programme_id = p.id;`
 
-        const admins = await pool.query(query, []);
-
-        console.log("SQL response: ", admins.rows);
+        const admins = await pool.query(query);
 
         return admins.rows;
-
     } catch (error) {
         console.error("Error fetching all admins (SQL):", error);
+        return []
     }
 }
 
--- types/citizen.ts
+++ types/citizen.ts
@@ -14,5 +14,6 @@ export interface ReturnedCitizen {
     name: string,
     email: string,
     gender: string,
+    programme: string,
     phoneNumber: string,
 }
