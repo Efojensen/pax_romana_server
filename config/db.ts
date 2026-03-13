4fc0976b11c8395b14eaefcddeef0ae168ee604b
EfoJensen <Jensena231@gmail.com>
2026-03-13 11:25:06 +0000

    trying out queries on built local postgres database

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,21 @@
 import pg from "pg";
 
+// const pool = new pg.Pool({
+//     user: process.env['SUPABASE_DB_USER'],
+//     host: process.env['SUPABASE_DB_HOST'],
+//     database: process.env['SUPABASE_DB_NAME'],
+//     password: process.env['SUPABASE_DB_PASSWORD'],
+//     port: parseInt(process.env['SUPABASE_DB_PORT']!),
+//     // connectionString: process.env['']!
+//     ssl: { rejectUnauthorized: false}
+// })
+
 const pool = new pg.Pool({
-    user: process.env['SUPABASE_DB_USER'],
-    host: process.env['SUPABASE_DB_HOST'],
-    database: process.env['SUPABASE_DB_NAME'],
-    password: process.env['SUPABASE_DB_PASSWORD'],
-    port: parseInt(process.env['SUPABASE_DB_PORT']!),
+    user: process.env['DB_USER'],
+    host: process.env['DB_HOST'],
+    database: process.env['DB_NAME'],
+    password: process.env['DB_PASSWORD'],
+    port: parseInt(process.env['DB_PORT']!),
     // connectionString: process.env['']!
     ssl: { rejectUnauthorized: false}
 })
