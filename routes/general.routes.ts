135bbd6eb9178416adc5fb194ff13a6418791c6a
EfoJensen <Jensena231@gmail.com>
2026-03-22 23:14:29 +0000

    created and used the validate_jwt middleware

--- routes/general.routes.ts
+++ routes/general.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import {validateJwt} from '../middlewares/validate_jwt';
 import { createAnnouncementController } from '../controllers/general/announcements/createAnnouncement.controller';
 import { getAllAnnouncementController } from '../controllers/general/announcements/getAllAnnouncements.controller';
 
 const router = Router();
 
 router.get('/announcements', getAllAnnouncementController);
-router.post('/announcements/new', createAnnouncementController);
+router.post('/announcements/new', validateJwt,createAnnouncementController);
 
 export default router;
